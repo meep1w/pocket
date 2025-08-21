443cdd6e561bc652430ad6b53fb00388a0d58140
meep1w <kerentsev05@inbox.ru>
2025-08-21 09:41:45 +0300

    мейн

--- app/db.py
+++ app/db.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from pathlib import Path
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///pocketbot.db"
+# абсолютный путь к файлу БД рядом с проектом
+DB_PATH = (Path(__file__).resolve().parent.parent / "pocketbot.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
